6664163d2d5888e4d510c5faa77c54b4cc054485
tucker <tuckerjamesmayo@gmail.com>
2015-03-11 16:30:20 -0700

    change router

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
 Budget.Router.map(function() {
   this.resource('countries', {path: '/'});
   this.resource('country', {path: ':country_id'}, function() {
-          this.resource('categories');
+          // this.resource('categories');
           this.resource('category', {path: ':category_id'});
           // function() {
            this.resource('new-item');
@@ -9,6 +9,7 @@ Budget.Router.map(function() {
   // this.resource('categories', {path: ':countries_id'});
   // this.resource('category', {path: ':category_id'},
   // this.resource('country', {path: ':country_id'},
+this.resource('categories');
 
 
 // };
