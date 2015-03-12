1573c7f7e65f4c41d3a63cbc93feeff2d2e25404
tucker <tuckerjamesmayo@gmail.com>
2015-03-12 15:56:48 -0700

    commit beginnings of carousel only partially completed

---
+++ controllers/application.js
@@ -0,0 +1,37 @@
+Budget.ApplicationController = Ember.Controller.extend({
+
+  pictures: [
+    'backpackingpoll',
+    'Chinatown-Bangkok-Thailand',
+    'int-mountain-day',
+    'rock-climb',
+    'lesedi_village_1',
+    'cappadocia-turkey',
+    'parisguidetower',
+    'Thai-Street-Food',
+    'wat-ban-den'
+    ],
+//   current: 0,
+//   storyImage: '/css/img/rock-climb.jpg',
+//   actions: {
+//     showOldPic: function() {
+//       if (this.get('current') === 0){
+//         this.set('current', this.get('pictures').length - 1);
+//       } else {
+//         this.set('current', this.get('current') - 1);
+//       }
+//       this.set('storyImage', "/css/img/" + this.get('pictures')[this.get('current')] + ".jpg")
+//     },
+//
+//     showNewPic: function() {
+//       if (this.get('current') === this.get('pictures').length - 1){
+//         this.set('current', 0);
+//       } else {
+//         this.set('current', this.get('current') + 1);
+//       }
+//       this.set('storyImage', "/css/img/" + this.get('pictures')[this.get('current')] + ".jpg")
+//     },
+//
+//   }
+//
+});
