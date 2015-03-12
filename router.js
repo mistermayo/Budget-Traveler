3a1c5320d92e9e1b738a59788c8f14667bbb183f
tucker <tuckerjamesmayo@gmail.com>
2015-03-12 11:13:45 -0700

    attempt to refactor router so that country button doesnt show when creating new item, unfixed issue

--- router.js
+++ router.js
@@ -4,13 +4,10 @@ Budget.Router.map(function() {
     this.resource('new-country');
     this.resource('new-item');
     this.resource('item');
-
-    });
+  });
     this.resource('categories');
     this.resource('category', {path: ':category_id'});
-  this.resource('new-category');
-
-  this.resource('item', {path: ':item_id'});
-
-  this.resource('about');
+    this.resource('new-category');
+    this.resource('item', {path: ':item_id'});
+    this.resource('about');
 });
