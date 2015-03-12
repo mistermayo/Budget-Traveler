4b57a4bc6cb0473599a3b1e61e79cf2b5d55170c
tucker <tuckerjamesmayo@gmail.com>
2015-03-12 10:01:34 -0700

    add image to country feature, refactor templates, models and controller accordingly

--- controllers/new-country.js
+++ controllers/new-country.js
@@ -2,13 +2,17 @@ Budget.NewCountryController = Ember.Controller.extend({
   actions: {
     save: function() {
       var newCountry = this.store.createRecord('country', {
-        name: this.get('name')
+        name: this.get('name'),
+        image: this.get('image')
+
       });
 
       newCountry.save();
       this.transitionToRoute('countries');
       this.set("name", null);
       this.set("country", null);
+      this.set('image', null);
+
     }
 
   }
--- models/country.js
+++ models/country.js
@@ -1,4 +1,5 @@
 Budget.Country = DS.Model.extend({
   name: DS.attr(),
+  image: DS.attr(),
   categories: DS.hasMany('category', {async: true})
 });
